127c8cdc3f1ec7500c1b3d249706c57217616f90
Robin Chow <robinc@robinchow.net>
2018-03-16 07:56:04 +0000

    Integrated app into a waitress server

--- run_server.py
+++ run_server.py
@@ -1,3 +1,19 @@
+from waitress import serve
 from app import app
-app.run(debug=True)
+
+import os
+import logging
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+logs_dir = os.path.join(script_dir, 'logs')
+
+if not os.path.exists(logs_dir):
+    os.mkdir(logs_dir)
+
+logging.basicConfig(filename=os.path.join(logs_dir, 'server.log'),
+    format='%(asctime)s %(levelname)s %(message)s',
+    level=logging.DEBUG)
+
+if __name__ == '__main__':
+    serve(app, host='*', port=8200)
 
